d1c6c1d6bcddab1f45e038b94a7dcd267b67b571
FabriceBlacksheep <fabrice@blacksheep-van.com>
2023-02-13 17:55:21 +0100

    update

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
